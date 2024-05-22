4f0c2ac676416ac3efae566d2d20f20df18b4381
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-22 21:32:18 +0200

    Update manegar.h

--- manegar.h
+++ manegar.h
@@ -7,3 +7,4 @@
 Salle* creer_salle(const char* nom, int nombre_rangees, int sieges_par_rangee);
 void configurer_categories(Salle* salle, int nombre_rangees_a, int nombre_rangees_b);
 Concert* attribuer_concert(Salle* salle, const char* nom, int a_fosse, double prix_a, double prix_b, double prix_c, const char* heure_fin);
+void afficher_etat_salle(Salle* salle) ;
