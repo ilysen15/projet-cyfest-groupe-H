7773a631db1ef29113ccdcd133d2ba6e46b8400a
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-22 22:57:40 +0200

    Create sauvgard.c

---
+++ sauvgard.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"sauvgard.h"
+#include "manegar.h"
+
+
+void enrigistresalle(const Salle* salle) {
+  FILE *file;
+  recovertext();
+
+  remove("save.txt");//supprime le contenue du file
+  file=fopen("save.txt","w");
+     if (file == NULL) {
+        printf("Could not open file.\n");
+        exit(0);
+    }
+  fprintf(file, "%s", recovertext(partie));
+  fclose(file);
+  printf("Votre partie a bien ete enregistre!");
+}
+
+void restartGame(const char* partie){
+  displayPart(partie);
+}
