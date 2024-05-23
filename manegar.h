f506e308cd876f6ce058cee3f3366ac2add6a9fc
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-23 22:42:41 +0200

    Update manegar.h

--- manegar.h
+++ manegar.h
@@ -4,7 +4,7 @@
 #define TAILLE 100
 
 
-Salle* creer_salle(const char* nom, int nombre_rangees, int sieges_par_rangee);
+Salle *creer_salle(){
 Concert* attribuer_concert(Salle* salle, const char* nom, int a_fosse, double prix_a, double prix_b, double prix_c, const char* heure_fin);
 void afficher_etat_salle(Salle* salle) ;
 void liberer_salle(Salle* salle) ;
