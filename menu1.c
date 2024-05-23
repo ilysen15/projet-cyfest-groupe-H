5bcf2048e2db990f103d50cb37d7c1de8f366b1c
ilysen15 <ilysen15@gmail.com>
2024-05-23 14:34:25 +0200

    Update menu1.c

--- menu1.c
+++ menu1.c
@@ -11,8 +11,48 @@ void afficherMenu() {
 }
 
 
-void modeManager() {
-    printf("\n--- Mode Manager ---\n");
+int modeManager(void){
+int choix;
+    printf("=== Mode Manager ===\n");
+ do {
+        printf("Menu \n");
+        printf("choissiez 1 pour salle de classe A \n");
+        printf("choissiez 2 pour salle de classe B \n");
+        printf("choissiez 1 pour salle de classe C \n");
+        printf("choissiez 4 pour creer une salle \n");
+        printf("Entrez votre choix : ");
+        scanf("%d", &choix);
+        while(getchar()!='\n'){
+          
+        }
+   
+
+        switch (choix) {
+            case 1:
+                fopen( "salle1.txt");
+             
+
+                break;
+            case 2:
+                fopen("salle2.txt");
+              
+                break;
+               
+            case 3:
+               fopen("salle3.txt");
+                exit(0);
+                break;
+            case 4:
+               fopen("salle3.txt");
+                exit(0);
+                break;
+            default:
+                printf("Choix invalide. Veuillez entrer un nombre entre 1 et 4.\n");
+                break;
+        }
+    } while ((choix<0 || choix >4) || getchar()!='\n');
+
+    return 0;
 }
 
 
