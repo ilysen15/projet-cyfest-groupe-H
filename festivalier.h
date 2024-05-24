2045a17147ddb591ae8de5854bd90cdaff4630b1
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-24 15:48:45 +0200

    Update festivalier.h

--- festivalier.h
+++ festivalier.h
@@ -41,4 +41,8 @@ typedef struct {
     Date date_fin;           // Date de fin du concert
 } Concert;
 Salle* lire_salle(const char* nom_fichier) ;
+void afficher_sieges_salle(Salle* salle) ;
+float reserver_sieges(Salle* salle, Concert* concert, int rangee, int siege) ;
+int concert_en_cours(Concert* concert, Date date_courante, Heure heure_courante) ;
+void afficher_concerts_en_cours(Concert* concerts, int nombre_concerts, Date date_courante, Heure heure_courante) ;
 
