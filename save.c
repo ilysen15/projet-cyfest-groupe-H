00422c50ffe534b799407ebfda14db0979ab9613
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-24 15:13:50 +0200

    Update save.c

--- save.c
+++ save.c
@@ -4,20 +4,3 @@
 #include "maneger.h"
 
 
-void enrigistresalle(const Salle* salle) {
-  FILE *file;
-  ();
-
-  remove("save.txt");//supprime le contenue du file
-  file=fopen("save.txt","w");
-     if (file == NULL) {
-        printf("fichier n'existe pas.\n");
-        exit(0);
-    }
-  fprintf(file, "%s", (salle));
-  fclose(file);
-  printf("Votre partie a bien ete enregistre!");
-}
-
-
-
