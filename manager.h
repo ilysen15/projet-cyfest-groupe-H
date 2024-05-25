30178c9a9bb514e2c3eb895d99a392dc919f7813
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-25 20:47:02 +0200

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,4 +1,3 @@
-
 #include "header.h"
 
 void Listesalle();
@@ -7,4 +6,3 @@ Salle* creer_salle();
 Concert* attribuer_concert(Salle* salle);
 void afficher_etat_salle(Salle* salle);
 int choix_manager();
-
