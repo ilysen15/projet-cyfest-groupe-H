4ce9bf12a618e609ba100dc330777f43efe60a4c
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-25 20:45:01 +0200

    Update festivalier.h

--- festivalier.h
+++ festivalier.h
@@ -6,4 +6,3 @@ float reserver_sieges(Salle* salle, Concert* concert, int rangee, int siege);
 int concert_en_cours(Concert* concert, Date date_courante, Heure heure_courante);
 void afficher_concerts_en_cours(Concert* concerts, int nombre_concerts, Date date_courante, Heure heure_courante);
 int choix_festivalier();
-
