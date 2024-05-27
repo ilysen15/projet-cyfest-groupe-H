d0dd71d0d923e9f0789efa8fd57462def8e0ba13
attiakarin <134232256+attiakarin@users.noreply.github.com>
2024-05-27 12:48:40 +0200

    Delete manager.h

--- manager.h
+++
@@ -1,8 +0,0 @@
-#include "header.h"
-
-void Listesalle();
-void lire_et_afficher(FILE *f);
-Salle* creer_salle();
-Concert* attribuer_concert(Salle* salle);
-void afficher_etat_salle(Salle* salle);
-int choix_manager();
